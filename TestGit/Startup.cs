e227bf40d821c11bc59df4e1df3bf532c41a366f
Wasan <wasan.lu@baac.or.th>
2016-05-13 15:51:57 +0700

    Add project files.

---
+++ TestGit/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestGit.Startup))]
+namespace TestGit
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
