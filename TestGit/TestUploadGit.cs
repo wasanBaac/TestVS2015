8114b27f51329554152b4caec2412e3cc9f2dbc2
Wasan <wasan.lu@baac.or.th>
2016-05-16 13:30:46 +0700

    Update 212

--- TestGit/TestUploadGit.cs
+++ TestGit/TestUploadGit.cs
@@ -13,7 +13,7 @@ namespace TestGit
         }
         public static int GetNumber2()
         {
-            return 21;
+            return 212;
         }
         public static int GetNumber3()
         {
